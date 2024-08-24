953d1e848bc84c854121bfb24bd63785098456e3
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-08-24 22:14:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class gfg {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            int w = Integer.parseInt(br.readLine());
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            List<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] val = new int[array.size()];
+            int idx = 0;
+            for (int i : array) val[idx++] = i;
+
+            String lin = br.readLine();
+            String[] toke = lin.split(" ");
+            List<Integer> array1 = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : toke) {
+                array1.add(Integer.parseInt(token));
+            }
+
+            int[] wt = new int[array1.size()];
+            idx = 0;
+            for (int i : array1) wt[idx++] = i;
+
+            // calling method knapSack() of class Solution
+            System.out.println(new Solution().knapSack(w, wt, val));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int W, int wt[], int val[]) {
+        // your code here
+        int n = val.length;
+        int dp[][] = new int[n][W + 1];
+        
+        // Base Condition
+        for (int i = wt[0]; i <= W; i++) {
+            dp[0][i] = val[0];
+        }
+        
+        for (int ind = 1; ind < n; ind++) {
+            for (int cap = 0; cap <= W; cap++) {
+                // Calculate the maximum value when the current item is not taken
+                int notTaken = dp[ind - 1][cap];
+                
+                // Calculate the maximum value when the current item is taken
+                int taken = Integer.MIN_VALUE;
+                if (wt[ind] <= cap) {
+                    taken = val[ind] + dp[ind - 1][cap - wt[ind]];
+                }
+                
+                // Store the maximum value for the current state
+                dp[ind][cap] = Math.max(notTaken, taken);
+            }
+        }
+        
+        return dp[n - 1][W];
+    }
+}
