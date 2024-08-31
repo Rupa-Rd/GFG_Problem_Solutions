0734da7a7bed856b19c47bdd8086035c96487718
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-08-31 16:27:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of Two Sorted Arrays/union-of-two-sorted-arrays.java
@@ -0,0 +1,124 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t  =Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String st[] = read.readLine().trim().split(" ");
+            int N = Integer.parseInt(st[0]);
+            int M = Integer.parseInt(st[1]);
+            
+            int arr1[] = new int[N];
+            int arr2[] = new int[M];
+            
+            st = read.readLine().trim().split(" ");
+            for(int i = 0; i < N; i++)
+              arr1[i] = Integer.parseInt(st[i]);
+            
+            st = read.readLine().trim().split(" ");  
+            for(int i = 0; i< M; i++)
+              arr2[i] = Integer.parseInt(st[i]);
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = obj.findUnion(arr1, arr2, N, M);
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println(); 
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//arr1,arr2 : the arrays
+// n, m: size of arrays
+class Solution
+{
+    //Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        // add your code here
+        // ArrayList<Integer> ans = new ArrayList<>();
+        // int i = 0, j = 0;
+        
+        // while(i < n && j < m){
+        //     if(arr1[i] <= arr2[j]){
+        //         if(ans.size() == 0 || !ans.contains(arr1[i])){
+        //             ans.add(arr1[i]);
+                 
+        //         }
+        //         i++;
+        //     }else{
+        //         if(ans.size() == 0 || !ans.contains(arr2[j])){
+        //             ans.add(arr2[j]);
+                
+        //         }
+        //         j++;
+        //     }
+        // }
+        
+        // // Remaining elements in arr1
+        // while(i < n){
+        //     if(!ans.contains(arr1[i]))
+        //         ans.add(arr1[i]);
+        //     i++;
+        // }
+        
+        // // Remaining elements in arr2
+        // while(j < m){
+        //     if(!ans.contains(arr2[j]))
+        //         ans.add(arr2[j]);
+        //     j++;
+        // }
+        
+        // return ans;
+        int i = 0, j = 0; // pointers
+  ArrayList<Integer > Union=new ArrayList<>(); // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.get(Union.size()-1) != arr1[i])
+        Union.add(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.get(Union.size()-1) != arr2[j])
+        Union.add(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.get(Union.size()-1) != arr1[i])
+      Union.add(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.get(Union.size()-1) != arr2[j])
+      Union.add(arr2[j]);
+    j++;
+  }
+  return Union;
+    }
+}
+
+
+
