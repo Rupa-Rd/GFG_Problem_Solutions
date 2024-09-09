5f8398d6f4fc3cdc839c70de565ede2610d85408
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-09-09 19:27:09 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String input = br.readLine();
+            String[] inputArray = input.split("\\s+");
+            ArrayList<Integer> a = new ArrayList<>();
+
+            for (String s : inputArray) {
+                a.add(Integer.parseInt(s));
+            }
+
+            Solution ob = new Solution();
+            ob.sort012(a);
+
+            for (int num : a) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to sort an array of 0s, 1s, and 2s
+    public void sort012(ArrayList<Integer> arr) {
+        // code here
+        int zeroCount = 0, oneCount = 0, twoCount = 0;
+        for(int i: arr){
+            if(i == 0)
+            zeroCount ++;
+            else if(i == 1)
+            oneCount ++;
+            else
+            twoCount++;
+        }
+        
+        int i = 0, n = arr.size();
+        
+        while(i < zeroCount){
+            arr.set(i, 0);
+            i++;
+        }
+        
+        while(i < zeroCount + oneCount){
+            arr.set(i, 1);
+            i++;
+        }
+        while(i < n){
+            arr.set(i, 2);
+            i++;
+        }
+        
+    }
+}
